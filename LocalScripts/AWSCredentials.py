fe75de4b7148a325ad0e5b80c90549e88f2c8e17
yoav freund <yoav.freund@gmail.com>
2014-04-17 14:02:40 -0700

    Delete AWSCredentials.py

--- LocalScripts/AWSCredentials.py
+++
@@ -1,7 +0,0 @@
-### AWS credentials: ####
-aws_access_key_id='AKIAI7AWJT7RBAPJX6WQ'
-aws_secret_access_key='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'
-keyPairFile='/Users/yoavfreund/.ssh/YoavsKeypair.pem' # name of local file storing keypair
-key_name="YoavsKeypair"         # name of keypair on AWS
-security_groups=['YoavsWork']
-### End of AWS credentials ####
