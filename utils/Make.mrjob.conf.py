fc3f7d9f96904eb8d724d185707c51552670b9dc
yoav freund <yfreund@ucsd.edu>
2014-05-19 14:26:30 -0700

    Simplified mrjob.conf configuration

--- utils/Make.mrjob.conf.py
+++ utils/Make.mrjob.conf.py
@@ -1,18 +1,28 @@
-" Insert credentials into mrjob configuration file "
+""" Insert credentials into mrjob configuration file """
 import sys, os, pickle
-
+                    
 if 'EC2_VAULT' in os.environ.keys():
     vault=os.environ['EC2_VAULT']
 else:  # If EC2_VAULT is not defined, we assume we are in an EC2 instance
     vault='/home/ubuntu/Vault'
 try:
-    with open(vault+'/Creds.pkl') as file:
+    vaultname=vault+'/Creds.pkl'
+    def check(key,Dict ):
+        if not key in Dict.keys():
+            sys.exit('The file: '+vaultname+' Does not contain the key "'+\
+                     key+'" in the correct place"')
+
+    with open(vaultname) as file:
         Creds=pickle.load(file)
-    print 'Creds=',Creds
+
+    check('mrjob',Creds)
     keypair=Creds['mrjob']
-    print 'keypair=',keypair
     template=open('mrjob.conf.template').read()
+
+    check('key_id',keypair)
+    check('secret_key',keypair)
     filled= template % (keypair['key_id'],keypair['secret_key'])
+    # print 'filled=\n',filled
     home=os.environ['HOME']
     outfile = home+'/.mrjob.conf'
     open(outfile,'wb').write(filled)
